8c1e0ee4c29a6e1f6f307a12348874d9a00adc7e
THAMIZHVENDHAN V <727724euai163@skcet.ac.in>
2026-07-24 11:09:41 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/AlertNotificationRepository.java
+++ backend/src/main/java/com/example/demo/repository/AlertNotificationRepository.java
@@ -4,10 +4,13 @@ import com.example.demo.entity.AlertNotification;
 import com.example.demo.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface AlertNotificationRepository extends JpaRepository<AlertNotification, Long> {
+    
     List<AlertNotification> findByTargetRole(Role role);
     List<AlertNotification> findByTargetRoleAndIsReadFalse(Role role);
+    List<AlertNotification> findByIsReadFalse();
 }
